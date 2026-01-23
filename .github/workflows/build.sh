3ffc8e03f463ecaeb31c4ca6f68edaafd111c26e
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2026-01-23 13:07:11 -0500

    .

---
+++ .github/workflows/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+: ${REVERB_COMPOSE_FILE:=compose.yaml}
+: ${REVERB_SERVICES:=}
+: ${REVERB_DOCKER_BUILD_CACHE_DIR:=/tmp/docker-build-cache}
+: ${DEBUG:=0}
+if [ ${DEBUG} != 0 ]; then set -x; fi
+export DOCKER_BUILDKIT=1
+
+die() { echo "${1:-}" >&2; exit 1; }
+log() { echo "$@" >&2; }
+
+#============================================================
+
+initialize() {
+  docker compose version
+  initializeComposeFile
+  initializeServices
+}
+
+initializeComposeFile() {
+  if [ ! -f ${REVERB_COMPOSE_FILE} ]; then die "Compose file not found: ${REVERB_COMPOSE_FILE}"; fi
+  log "Using compose file: ${REVERB_COMPOSE_FILE}"
+}
+
+initializeServices() {
+  if [ -n "${REVERB_SERVICES:-}" ]; then return; fi
+  REVERB_SERVICES=$(docker compose config --format json \
+    | jq -r '.services|to_entries[]|select(.value.build != null and .value.image != null)|.key' \
+    | paste -sd ' ')
+  if [ -z "${REVERB_SERVICES:-}" ]; then die "No services found to publish. Add service.*.image and service.*.build information."; fi
+  log "Services: ${REVERB_SERVICES}"
+}
+
+build() {
+  log "Building images"
+  docker compose build
+}
+
+release() {
+  docker images
+  log "Pushing images ..."
+  docker compose push -q $REVERB_SERVICES
+}
+
+#============================================================
+
+main() {
+  initialize
+  case "${1:-help}" in
+    b*) build;;
+    r*) release;;
+    *) die "Usage: $0 <build|release>";;
+  esac
+  log "Done"
+}
+main $@
---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM busybox:stable AS base
+ENV PLAYERONE_ID=999
+RUN <<EOF
+  addgroup -g $PLAYERONE_ID playerone
+  adduser -S -u $PLAYERONE_ID -G playerone playerone
+EOF
+USER playerone
+ENTRYPOINT [ "sh" ]
+
+FROM base AS builder
+ADD --chown=${PLAYERONE_ID}:${PLAYERONE_ID} . /home/playerone/project/
+WORKDIR /home/playerone/project
+RUN <<EOF
+ date > build.txt
+ echo $RANDOM >> build.txt
+EOF
+
+FROM base AS app
+COPY --from=builder /home/playerone/project/build.txt /
